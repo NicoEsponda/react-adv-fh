0e50555d42c26a61f43a19756b128974b55c1710
Nicolás Esponda <nicolas.esponda@hotmail.com>
2023-04-21 09:31:00 -0400

    Primer commit listo

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import logo from '../assets/react.svg'
+
+export const Navigation = () => {
+    return (
+
+        <BrowserRouter>
+            <div className='main-layout'>
+                <nav>
+                    <img src={logo} alt="react logo" />
+
+                    <ul>
+                        <li>
+                            <NavLink to='/home' className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/about' className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/users' className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path='/home' element={<h1>Home Page</h1>}></Route>
+                    <Route path='about' element={<h1>About Page</h1>}></Route>
+                    <Route path='users' element={<h1>Users Page</h1>}></Route>
+                    <Route path='/*' element={<Navigate to='/home replace' />}></Route>
+                    {/* <Route path='about' element={<About />}></Route> */}
+                    {/* <Route path='users' element={}></Route> */}
+                </Routes>
+            </div>
+        </BrowserRouter>
+
+    )
+}
